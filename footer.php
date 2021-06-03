e58263417b58aada4e725e245216388b96f3ac56
Jia-09 <79925230+Jia-09@users.noreply.github.com>
2021-06-04 06:49:08 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,32 @@
+
+    <footer class=" container-fluid text-center">
+        <div class="container">
+            <div class="row">
+        <div class="col-md-6 text-left">
+
+    <ul class="footer">
+      <li><a href="#">London</a></li>
+      <li><a href="#">Paris</a></li>
+      <li><a href="#">Tokyo</a></li>
+    <li><a href="#">Tokyo</a></li>
+        <li><a href="#">Paris</a></li>
+      <li><a href="#">Tokyo</a></li>
+    <li><a href="#">Tokyo</a></li>
+    </ul>
+  
+                </div>
+
+        <div class="col-md-6 text-left">
+            <div class="col-sm-6 form-group">
+            <h7> EMAIL US</h7>
+          <input class="form-control" id="email" name="email" placeholder="Send Us Email..." type="email" required>
+            <button class=" col-sm-6 form-group btn pull-right" type="submit">Send</button>
+                </div>
+             </div>    
+                </div><!--row-->
+             </div> <!--container-->
+    </footer>
+
+</body>
+</html>
+<?php wp_footer(); ?>
